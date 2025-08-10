db25b61a4a0f044f3d1063d922c3e6fba99b2a50
duble35 <dambleerley@yahoo.com>
2025-08-10 13:03:27 -0300

    Adicionando o springboot

---
+++ grifo/src/main/java/com/br/grifo/GrifoApplication.java
@@ -0,0 +1,13 @@
+package com.br.grifo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrifoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrifoApplication.class, args);
+	}
+
+}
---
+++ grifo/src/test/java/com/br/grifo/GrifoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.grifo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrifoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
