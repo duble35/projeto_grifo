7fdeefe9eb9d8a1b85bbfe9181611d4630ee7ceb
duble35 <dambleerley@yahoo.com>
2025-08-10 17:27:01 -0300

    Atualização do modelo Hardware

--- grifo/src/main/java/com/br/grifo/model/Hardware.java
+++ grifo/src/main/java/com/br/grifo/model/Hardware.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Hardware {
@@ -13,82 +15,69 @@ public class Hardware {
 
     private String serialNumber;
     private String model;
-    private String manufacturer;
+    @ManyToOne
+    @JoinColumn(name = "manufacturer_id", nullable = false)
+    private Manufacturer manufacturer;
     private String type;
     private String macAddress;
     private String assetTag;
     private String location;
     private String purchaseDate;
-
+    
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getSerialNumber() {
         return serialNumber;
     }
-
     public void setSerialNumber(String serialNumber) {
         this.serialNumber = serialNumber;
     }
-
     public String getModel() {
         return model;
     }
-
     public void setModel(String model) {
         this.model = model;
     }
-
-    public String getManufacturer() {
+    public Manufacturer getManufacturer() {
         return manufacturer;
     }
-
-    public void setManufacturer(String manufacturer) {
+    public void setManufacturer(Manufacturer manufacturer) {
         this.manufacturer = manufacturer;
     }
-
     public String getType() {
         return type;
     }
-
     public void setType(String type) {
         this.type = type;
     }
-
     public String getMacAddress() {
         return macAddress;
     }
-
     public void setMacAddress(String macAddress) {
         this.macAddress = macAddress;
     }
-
     public String getAssetTag() {
         return assetTag;
     }
-
     public void setAssetTag(String assetTag) {
         this.assetTag = assetTag;
     }
-
     public String getLocation() {
         return location;
     }
-
     public void setLocation(String location) {
         this.location = location;
     }
-
     public String getPurchaseDate() {
         return purchaseDate;
     }
-
     public void setPurchaseDate(String purchaseDate) {
         this.purchaseDate = purchaseDate;
     }
+
+    
 }
