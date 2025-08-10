b5f05174553a2422c4fd4e44b68fc5dae1f25d3a
duble35 <dambleerley@yahoo.com>
2025-08-10 19:51:11 -0300

    Atuliização de do controller Model

--- grifo/src/main/java/com/br/grifo/controller/ModelController.java
+++ grifo/src/main/java/com/br/grifo/controller/ModelController.java
@@ -4,14 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.br.grifo.model.ModelHardware;
 import com.br.grifo.repository.ModelRepository;
-
 
 @RestController
 @RequestMapping("/model")
@@ -25,11 +21,24 @@ public class ModelController {
         return modelRepository.findAll();
     }
     
-    @GetMapping
+    @GetMapping("/{id}")
     public Optional<ModelHardware> getModelById(@PathVariable Long id) {
         return modelRepository.findById(id);
     }
 
-    
+    @PostMapping
+    public ModelHardware createModel(@RequestBody ModelHardware model) {
+        return modelRepository.save(model);
+    }
 
+    @PutMapping("/{id}")
+    public ModelHardware updateModel(@PathVariable Long id, @RequestBody ModelHardware model) {
+        model.setId(id);
+        return modelRepository.save(model);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteModel(@PathVariable Long id) {
+        modelRepository.deleteById(id);
+    }
 }
