9e3fb205021f2f3055fb86c749e1df6f5587e4f9
duble35 <dambleerley@yahoo.com>
2025-08-15 11:13:04 -0300

    Atualização da página agenda

--- gui/src/HomeModel.js
+++ gui/src/HomeModel.js
@@ -5,14 +5,14 @@ export default function HomeModel({ title, children }) {
     return (
         <div className="text-gray-800">
             <nav className="py-5 bg-white">
-                <div className="container mx-auto w-[960px] flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
+                <div className="px-5 flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
                     <h1 className="text-3xl font-bold text-gray-700">
-                        <img src={Logo} alt="Logo do sistema" className="h-[50px]" />
+                        <img src={Logo} alt="Logo do sistema" className="h-[40px]" />
                     </h1>
                     <ul className="flex gap-4">
                         <li><a href="/" className="hover:underline">Agenda</a></li>
                         <li><a href="/#" className="hover:underline">CEP/Município</a></li>
-                        <li><a href="/#" className="bg-gray-700 hover:bg-gray-900 p-2 rounded text-white">Login</a></li>
+                        <li><a href="/#" className="hover:bg-gray-100 border border-gray-500 p-1 rounded-full flex gap-1"><i className="fa fa-user border w-6 h-6 text-center p-1 rounded-full bg-gray-700 text-white"></i><span className="font-bold px-1">Login</span></a></li>
                     </ul>
                 </div>
             </nav>
